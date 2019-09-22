a258a57c4f963a9113d452ce7501ae83cffaea74
yoelio <yoswed123@gmail.com>
2019-09-22 14:32:00 -0400

    wip FAQ page

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,17 @@
           GitHub
         </a>
       </div>
+      <div class="FAQ">
+      <h3 class="subtitle2">
+        Frequently asked questions
+      </h3> 
+      <body class="body">
+        What is a hackathon? <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. <br><br>
+        What if I don't know how to code?<br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.
+      </body>
+      </div>
     </div>
   </div>
 </template>
@@ -64,6 +75,15 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+}
+
+.subtitle2 {
+  font-weight: 300;
+  font-size: 25px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-top: 40px;
+  padding-bottom: 15px; 
 }
 
 .links {
